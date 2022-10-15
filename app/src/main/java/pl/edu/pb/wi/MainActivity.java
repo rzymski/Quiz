f640fee81593d36f79f534a23cf7f2c8d6e2d423
rzymski <83671265+rzymski@users.noreply.github.com>
2022-10-15 23:44:41 +0200

    2 zajecia

--- app/src/main/java/pl/edu/pb/wi/MainActivity.java
+++ app/src/main/java/pl/edu/pb/wi/MainActivity.java
@@ -1,12 +1,24 @@
 package pl.edu.pb.wi;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     private Button trueButton;
@@ -14,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private Button nextButton;
     private TextView questionTextView;
     private TextView punctation;
+    private Button promptButton;
+    private boolean answerWasShown;
 
     private Question[] questions = new Question[]{
             new Question(R.string.q_pizza, true),
@@ -25,13 +39,16 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private int currentIndex = 0;
-    private int actualPoints = 0, givenAnsewers = 0;
-    private boolean givenAnswer = false;
+    private int actualPoints = 0, givenAnswers = 0;
+    private boolean givenLastAnswer = false;
 
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer){
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
+        }
+        else if(userAnswer == correctAnswer){
             resultMessageId = R.string.correct_answer;
             actualPoints++;
         }
@@ -39,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             resultMessageId = R.string.incorrect_answer;
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
-        punctation.setText("Zdobyte punkty: "+actualPoints+"/"+givenAnsewers);
+        punctation.setText("Zdobyte punkty: "+actualPoints+"/"+givenAnswers);
     }
 
     private void setNextQuestion(){
@@ -47,9 +64,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onSTART");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onRESUME");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onPAUSE");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onSTOP");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onDESTROY");
+    }
+
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final String KEY_ACTUAL_POINTS = "actualPoints";
+    private static final String KEY_GIVEN_ANSWERS = "givenAnswers";
+    private static final String KEY_GIVEN_LAST_ANSWER = "givenLastAnswer";
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    public static final int REQUEST_CODE_PROMPT = 0;
+    ActivityResultLauncher<Intent> PromptActivityResultLauncher;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("QUIZ_TAG", "Wywolana zostala metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+        outState.putInt(KEY_ACTUAL_POINTS, actualPoints);
+        outState.putInt(KEY_GIVEN_ANSWERS, givenAnswers);
+        outState.putBoolean(KEY_GIVEN_LAST_ANSWER, givenLastAnswer);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity","Wywolana zostala metoda cyklu zycia: onCREATE");
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+            actualPoints = savedInstanceState.getInt(KEY_ACTUAL_POINTS);
+            givenAnswers = savedInstanceState.getInt(KEY_GIVEN_ANSWERS);
+            givenLastAnswer = savedInstanceState.getBoolean(KEY_GIVEN_LAST_ANSWER);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
@@ -57,12 +130,39 @@ public class MainActivity extends AppCompatActivity {
         questionTextView = findViewById(R.id.question_text_view);
         punctation = findViewById(R.id.actual_punctation);
 
+        ActivityResultLauncher<Intent> PromptActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == PromptActivity.RESULT_OK) {
+                            Intent data = result.getData();
+                            if(data == null) { return; }
+                            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+                        }
+                    }
+                });
+
+        promptButton = findViewById(R.id.promptButton);
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+                //startActivity(intent);
+                PromptActivityResultLauncher.launch(intent);
+            }
+        });
+
+        punctation.setText("Zdobyte punkty: "+ actualPoints +"/"+givenAnswers);
+
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(givenAnswer == false) {
-                    givenAnswer = true;
-                    givenAnsewers++;
+                if(givenLastAnswer == false) {
+                    givenLastAnswer = true;
+                    givenAnswers++;
                     checkAnswerCorrectness(true);
                 }
             }
@@ -71,9 +171,9 @@ public class MainActivity extends AppCompatActivity {
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(givenAnswer == false) {
-                    givenAnswer = true;
-                    givenAnsewers++;
+                if(givenLastAnswer == false) {
+                    givenLastAnswer = true;
+                    givenAnswers++;
                     checkAnswerCorrectness(false);
                 }
             }
@@ -82,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(givenAnswer == false) {
+                if(givenLastAnswer == false) {
                     return;
                 }
 
@@ -90,11 +190,12 @@ public class MainActivity extends AppCompatActivity {
                 if(currentIndex == questions.length){
                     currentIndex = 0;
                     actualPoints = 0;
-                    givenAnsewers = 0;
+                    givenAnswers = 0;
                 }
-                givenAnswer = false;
-                punctation.setText("Zdobyte punkty: "+ actualPoints +"/"+givenAnsewers);
+                givenLastAnswer = false;
+                punctation.setText("Zdobyte punkty: "+ actualPoints +"/"+givenAnswers);
                 //currentIndex = (currentIndex+1)%questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
---
+++ app/src/main/java/pl/edu/pb/wi/PromptActivity.java
@@ -0,0 +1,48 @@
+package pl.edu.pb.wi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PromptActivity extends AppCompatActivity {
+
+    private boolean correctAnswer;
+    private TextView wantSeeAnswerTextView;
+    private Button showPromptButton;
+    private TextView answerTextView;
+
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "pl.edu.pb.wi.quiz.answerShown";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("PromptActivity","PromptActivity Wywolana zostala metoda cyklu zycia: onCREATE");
+        setContentView(R.layout.activity_prompt);
+
+        wantSeeAnswerTextView = findViewById(R.id.wantSeeAnswerTextView);
+        showPromptButton = findViewById(R.id.showAnswerButton);
+        answerTextView = findViewById(R.id.answerTextView);
+
+        correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
+
+        showPromptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("PromptActivity","Wywolano: Click");
+                int answer = correctAnswer ? R.string.button_true : R.string.button_false;
+                answerTextView.setText(answer);
+                setAnswerShowResult(true);
+            }
+        });
+    }
+    private void setAnswerShowResult(boolean answerWasShown){
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
+    }
+}
